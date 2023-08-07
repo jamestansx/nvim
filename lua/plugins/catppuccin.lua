34f3f945b9d55fd86a3e5aa2e0862857d6183bb0
James Tan <jamestansx@gmail.com>
2023-08-07 22:44:45 +0800

    feat: add catppuccin colorscheme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,16 @@
+vim.g.catppuccin_flavour = "mocha"
+
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	init = function() vim.cmd.colorscheme("catppuccin") end,
+	opts = {
+		term_colors = true,
+		transparent_background = true,
+		highlight_overrides = {
+			all = function(colors) return {} end,
+		},
+	},
+}
