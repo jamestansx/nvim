e2e124b37cb96ead129f7abff6e3ea90cd748a90
James Tan <jamestansx@gmail.com>
2023-08-29 02:20:27 +0800

    feat: add fidget.nvim

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,10 +1,28 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile", "FileType" },
-	config = function()
-		for server, opts in pairs(require("plugins.lsp.servers")) do
-			opts = vim.tbl_extend("force", require("plugins.lsp.defaults"), opts)
-			require("lspconfig")[server].setup(opts)
-		end
-	end,
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile", "FileType" },
+		config = function()
+			for server, opts in pairs(require("plugins.lsp.servers")) do
+				opts = vim.tbl_extend("force", require("plugins.lsp.defaults"), opts)
+				require("lspconfig")[server].setup(opts)
+			end
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		tag = "legacy",
+		event = "LspAttach",
+		opts = {
+			text = { spinner = "dots" },
+			window = {
+				blend = 0,
+				relative = "editor",
+			},
+			timer = {
+				fidget_decay = 300,
+				task_decay = 300,
+			},
+		},
+	},
 }
